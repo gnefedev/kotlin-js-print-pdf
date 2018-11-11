1960c7fdfcd6af15bfb98cb2e669ef055c8e2533
Georgy Nefedev <gnefedev@yandex-team.ru>
2018-11-11 19:42:18 +0300

    draft

--- backend/src/main/kotlin/com/gnefedev/backend/Application.kt
+++ backend/src/main/kotlin/com/gnefedev/backend/Application.kt
@@ -1,7 +1,17 @@
 package com.gnefedev.backend
 
+import com.itextpdf.html2pdf.ConverterProperties
+import com.itextpdf.html2pdf.HtmlConverter
+import com.itextpdf.kernel.geom.PageSize
+import com.itextpdf.kernel.pdf.PdfDocument
+import com.itextpdf.kernel.pdf.PdfWriter
+import com.itextpdf.layout.font.FontProvider
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream
+import org.intellij.lang.annotations.Language
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.core.io.InputStreamResource
+import org.springframework.core.io.Resource
 import org.springframework.web.bind.annotation.RestController
 
 fun main(args: Array<String>) {
@@ -9,10 +19,49 @@ fun main(args: Array<String>) {
 }
 
 @SpringBootApplication
-class Application {
-}
+class Application
 
 @RestController
-class PdfController(
-) {
+class PdfController {
+    fun printTable(html: String): Resource = ByteOutputStream()
+        .use { outputStream ->
+            writePdf(outputStream, html)
+            val result = InputStreamResource(outputStream.newInputStream())
+            outputStream.flush()
+            result
+        }
+
+    fun writePdf(out: ByteOutputStream, html: String) {
+        val writer = PdfWriter(out)
+        val pdfDoc = PdfDocument(writer)
+
+        //Set the result to be tagged
+        pdfDoc.setTagged()
+        pdfDoc.defaultPageSize = PageSize.A4.rotate()
+
+        val converterProperties = ConverterProperties()
+
+        @Language("HTML")
+        val fullHtml = """
+            <!DOCTYPE html>
+<html lang="en" xmlns:âge-break-after="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="UTF-8">
+    <title>pdfHTML Accessibility example</title>
+    <style>
+        body {
+            font-size: 8px;
+        }
+    </style>
+</head>
+<body>$html</body>
+</html>
+        """.trimIndent()
+
+        //Create acroforms from text and button input fields
+        converterProperties.isCreateAcroForm = true
+
+        HtmlConverter.convertToPdf(fullHtml, pdfDoc, converterProperties)
+        pdfDoc.close()
+    }
 }
